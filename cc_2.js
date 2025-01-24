3fcf1096267f110b2f6844a0714bb720b195d8e9
Sophia Dolan <25srdolan@gmail.com>
2025-01-24 17:00:46 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -2,3 +2,10 @@ let products = ["shirt","pants","jacket","shorts","sweater"];
 products.unshift ("skirt");
 products.pop();
 console.log("Updated Product List:", products);
+
+let scores = [60, 70, 75, 95, 80];
+scores[1] = 85;
+let total = scores[0] + scores[1] + scores[2] + scores[3] + scores[4];
+let average = total / scores.length;
+console.log("Updated Scores:", scores);
+console.log("Average Score:", average);
