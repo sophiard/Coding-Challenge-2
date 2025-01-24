21f1f56ef026d88769af8039e095d9b1a8e74010
Sophia Dolan <25srdolan@gmail.com>
2025-01-24 17:13:26 -0500

    Task 3 - Employee Records

--- cc_2.js
+++ cc_2.js
@@ -9,3 +9,8 @@ let total = scores[0] + scores[1] + scores[2] + scores[3] + scores[4];
 let average = total / scores.length;
 console.log("Updated Scores:", scores);
 console.log("Average Score:", average);
+
+let employee = { name: "Anakin Skywalker", age: 25, department: "Jedi", isActive: true};
+
+employee.department = "Sith";
+console.log(employee);
